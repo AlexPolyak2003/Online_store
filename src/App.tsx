bf614e38d67c0740979f21c8c905d1ce73c88f5c
AlexPolyak2003 <polyanichko03@inbox.ru>
2023-09-20 16:48:35 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import styles from "./App.module.scss";
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}></div>
+    </div>
+  );
+};
+
+export default App;

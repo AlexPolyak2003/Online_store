75dae26e7d1ea77be5fd3d0d0b6feb352c951278
AlexPolyak2003 <polyanichko03@inbox.ru>
2023-10-18 13:05:46 +0300

    layout

--- src/components/header/head.tsx
+++ src/components/header/head.tsx
@@ -13,17 +13,27 @@ const Head = () => {
             <div className={styles.left_content}>
               <img src={phone} alt="phone" />
               <h1> +375 29 292-29-29 (VEL)</h1>
-              <h1> +375 33 333-33-33 (МТС)</h1>
+              <h2> +375 33 333-33-33 (МТС)</h2>
             </div>
             <div className={styles.middle_content}>
               <img src={location} alt="" />
               <h1>ваш город</h1>
             </div>
+
             <div className={styles.right_content}>
-              <img src={instagram} alt="" />
-              <h1>
-                посетите нашу <br /> страницу
-              </h1>
+              <div className={styles.login_position}>
+                <button className={styles.login}>Вход</button>
+              </div>
+              <div className={styles.registration_position}>
+                <button className={styles.registration}>Регистрация</button>
+              </div>
+
+              <div className={styles.visit_page}>
+                <img src={instagram} alt="" />
+                <h1>
+                  посетите нашу <br /> страницу
+                </h1>
+              </div>
             </div>
           </div>
         </div>
